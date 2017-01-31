ee418024ef4ae9ac2e83fc23efde26c540f23fea
lki1354 <lki1354@students.fhv.at>
2017-01-31 09:14:02 +0100

    * add two more example how to use the module * add new constructor to Enc class

--- src/examples/runIterationSelf.java
+++ src/examples/runIterationSelf.java
@@ -4,32 +4,31 @@ import ciayn.elements.Callable;
 import ciayn.elements.Input;
 import ciayn.elements.Output;
 import ciayn.elements.Updatable;
+import ciayn.elements.signal.Value;
 import ciayn.elements.signal.ValueFloat;
 import ciayn.environmantal.Env;
 import ciayn.environmantal.EnvController;
-import ciayn.environmantal.EnvPlant;
 
 /**
  * Created by lukas on 14.01.17.
  */
 
-public  class SimpleUse {
+public  class runIterationSelf {
         public static void main(String args[]) throws Exception {
-            float actualInputValue = 0;
+            Value actualInputValue = new ValueFloat(10);
 
             // callback this will be called if the controller updates the output
             Callable uActual = value -> System.out.println("u="+value.getValue()+" after "+value.getTimeStamp()+"sec");
             // this will be called when the controller starts a new iteration of calculation
-            Updatable xActual = () -> new ValueFloat(actualInputValue);
 
             Input w = new Input(new ValueFloat(2.0f));          //setpoint input of the controller
-            Input x = new Input(new ValueFloat(0f),xActual);            // actual value (feedback) from the plant
+            Input x = new Input(new ValueFloat(0f));            // actual value (feedback) from the plant
             Output u = new Output(new ValueFloat(0f),uActual);  // output of the controller
 
-            Env pid = EnvController.createEnvController(ValueFloat.class, 0.1f, 0.2f, 0.001f, w, x, u); //creates an PI controller
-            pid.setInterval(10);
+            Env pid = EnvController.createEnvPI(ValueFloat.class, 0.1f, 0.2f, 0.001f, w, x, u); //creates an PI controller
 
 
-            pid.run();
+            x.setValue(actualInputValue);
+            pid.runOneIteration();
         }
 }
